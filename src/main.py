5b7e363c75f1ffa94be7cebce517b008b1d82241
Frostbyte-Cat1 <87213468+AndrewRimington@users.noreply.github.com>
2021-07-17 23:31:59 +0800

    Update 0.9
    -Fixing sprite movement
    -Adding infinitely scrolling background
    -Adding Sci-Fi font
    -Editing score text
    -Adding surface to window

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ width,height = 750,600
 window = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Atlas's Voyage")
 
+
 #Sprites
 ##Debris
 rock1 = pygame.image.load(os.path.join("assets","rock1.png"))
@@ -33,21 +34,27 @@ class spaceship:
 
 def main():
     running = True
-    FPS = 60
+    FPS = 120
     clock = pygame.time.Clock()
-    vel = 0.4
-    player = spaceship(100,445)
-    
+    vel = 5
+    player = spaceship(90,365)
+    surface = pygame.Surface((750,600))
     score = 00000
 
     #To get sprites onto window surface
     def draw():
-        window.blit(bg,(0,0))
-        font = pygame.font.SysFont("monospace",30)
-        score_keeping = font.render(f"Score:{score}",1,(246,233,56))
-        window.blit(score_keeping,(10,10))
+        window.blit(surface,(0,0))
+
+        # surface.blit(bg,(0,0))
+
+        font = pygame.font.Font("assets\Aliens Among Us.ttf",25)
+        #Need to fix score adding system
+        text_surface = font.render(f"Score: {score}{score}{score}{score}",1,(255,255,255))
+        window.blit(text_surface,(10,10))
         player.draw_ship(window)
         pygame.display.update()
+
+    y = 0
 
     while running:
         clock.tick(FPS)
@@ -56,16 +63,26 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        rel_y = y % bg.get_rect().height
+        surface.blit(bg,(0,rel_y - bg.get_rect().height))
+        if rel_y < height:
+            draw()
+            surface.blit(bg,(0,rel_y))
+            y += 1
+            # pygame.draw.line()
+            pygame.display.update()
+
         
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and spaceship.x > vel + 5:
-            spaceship.x -= vel
+        if keys[pygame.K_a] and player.x - vel > -220:
+            player.x -= vel
         # vel + 5 = 5.4, this is so that the border spacing on the right side is the same as the left side
-        if keys[pygame.K_d] and spaceship.x < 720-width-5.4:
-            spaceship.x += vel
+        if keys[pygame.K_d] and player.x + vel < 415:
+            player.x += vel
         #elif so that by holding eg. d and left arrow keys together, player won't double in speed
-        elif keys[pygame.K_LEFT] and spaceship.x > vel + 5:
-            spaceship.x -= vel
-        elif keys[pygame.K_RIGHT] and spaceship.x < 720-width-5.4:
-            spaceship.x += vel
+        elif keys[pygame.K_LEFT] and player.x - vel > -220:
+            player.x -= vel
+        elif keys[pygame.K_RIGHT] and player.x + vel < 415:
+            player.x += vel
 main()
