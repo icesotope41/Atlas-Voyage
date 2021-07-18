839acfcd0eb56ed9f27e50b6e34e602d3fe2c175
Icesotope <53615543+AbsoluteZeroist@users.noreply.github.com>
2021-07-18 19:05:04 +0800

    fixed score counting.
    also felix was triggered at your spacings. I fixed those too.

--- src/main.py
+++ src/main.py
@@ -2,21 +2,21 @@ import pygame, os
 
 pygame.init()
 
-width,height = 750,600 
+width, height = 750, 600 
 window = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Atlas's Voyage")
 score = 0
 
 #Sprites
 ##Debris
-rock1 = pygame.image.load(os.path.join("assets","rock1.png"))
-rock2 = pygame.image.load(os.path.join("assets","rock2.png"))
+rock1 = pygame.image.load(os.path.join("assets", "rock1.png"))
+rock2 = pygame.image.load(os.path.join("assets", "rock2.png"))
 ##Spaceship
-atlas = pygame.image.load(os.path.join("assets","atlas.png"))
+atlas = pygame.image.load(os.path.join("assets", "atlas.png"))
 ##Laser beam
-laser = pygame.image.load(os.path.join("assets","laser_beam.png"))
+laser = pygame.image.load(os.path.join("assets", "laser_beam.png"))
 ##Background
-bg = pygame.transform.scale(pygame.image.load(os.path.join("assets","Background.png")),(width,height))
+bg = pygame.transform.scale(pygame.image.load(os.path.join("assets", "Background.png")),(width, height))
 
 
 class spaceship:
@@ -31,7 +31,7 @@ class spaceship:
 
 
     def draw_ship(self,window):
-        window.blit(self.ship_sprite,(self.x,self.y))
+        window.blit(self.ship_sprite, (self.x, self.y))
 
 
 def main():
@@ -39,33 +39,40 @@ def main():
     FPS = 120
     clock = pygame.time.Clock()
     vel = 5
-    player = spaceship(90,365)
-    surface = pygame.Surface((750,600))
+    player = spaceship(90, 365)
+    surface = pygame.Surface((750, 600))
     # score = 0
 
 
     #To get sprites onto window surface
     def draw():
-        global score
-        window.blit(surface,(0,0))
-        font = pygame.font.Font("assets\Aliens Among Us.ttf",25)
+        window.blit(surface,(0, 0))
+        font = pygame.font.Font("assets\Aliens Among Us.ttf", 25)
         #Need to fix score adding system
-        text_surface = font.render(f"Score: {score}",1,(255,255,255))
-        window.blit(text_surface,(10,10))
+        text_surface = font.render(f"Score: {score}", 1, (255,255,255))
+        window.blit(text_surface,(10, 10))
         player.draw_ship(window)
         pygame.display.update()
 
-
+    global framesPassed
+    framesPassed = 0 
     def score_keeping():
-            global score
+        global framesPassed, score
+        timeInterval = FPS/4 #amount of time passed before adding to score
+
+        if framesPassed == timeInterval: #condition to hit before adding to score. 
+            framesPassed = 0
             score += 1
 
-   
+        framesPassed += 1 #put here such that counter would start from one and end at time interval. 
+
+
     y = 0
     while running:
         clock.tick(FPS)
         draw()
-        score_keeping()
+        score_keeping() 
+        print(framesPassed)
         #To check event in the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
